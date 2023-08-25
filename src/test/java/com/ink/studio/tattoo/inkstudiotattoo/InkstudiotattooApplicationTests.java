f85e3deb6968c65fd38c332343106515f166e4c9
Gabriel Santos de Souza <rm86059@estudante.fieb.edu.br>
2023-08-25 12:03:12 -0300

    Criação do ambiente de desenvolvimento

---
+++ src/test/java/com/ink/studio/tattoo/inkstudiotattoo/InkstudiotattooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ink.studio.tattoo.inkstudiotattoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InkstudiotattooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
