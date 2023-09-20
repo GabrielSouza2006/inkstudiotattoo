732e1a9c6b954677b3ea3dc5c6e6073f2a75d1c7
Gabriel Santos <bielss1609@gmail.com>
2023-09-20 07:22:21 -0300

    Iniciação do controller

---
+++ src/main/java/com/ink/studio/tattoo/controller/lojaController.java
@@ -0,0 +1,16 @@
+package com.ink.studio.tattoo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ink.studio.tattoo.model.Loja;
+
+public class lojaController {
+
+}
